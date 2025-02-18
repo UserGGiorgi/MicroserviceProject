a23d6ec76eb590cd72a8e02c0e1821c78b313bae
Giorgi Abesadze <abesdzegiorgi@gmail.com>
2025-02-18 15:40:19 +0400

    +kubernetes

--- PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9449990f6322fc71d13cc1988b26d21960cc95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9566b45e358f90964d9209da790ce0ece70ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
